28600cb4a3ce837d0e734c373a4f727503643c7d
dhuynh1182 <d11huynh@torontomu.ca>
2023-09-30 20:52:31 -0400

    stratified data
    Ask about if step data is already catergorized

--- 850_PRO1.py
+++ 850_PRO1.py
@@ -5,4 +5,16 @@ import seaborn as sns
 import joblib
 import sklearn as sk
 
+from sklearn.model_selection import StratifiedShuffleSplit
 
+#importing data into Dataframe
+df = pd.read_csv("Project 1 Data.csv")
+
+#prep for strat. data
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=501)
+
+#strat. the data
+for train_index ,test_index in split.split(df,df["Step"]):
+    strat_train_set = df.loc[train_index].reset_index(drop=True)
+    strat_test_set = df.loc[test_index].reset_index(drop=True)
+    
