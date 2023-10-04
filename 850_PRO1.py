c08b69db1bf2f4ff4039fbc865b00cbc9cdab166
dhuynh1182 <d11huynh@torontomu.ca>
2023-10-03 20:33:11 -0400

    no.2
    added scalar, training set, and corr.

--- 850_PRO1.py
+++ 850_PRO1.py
@@ -6,15 +6,33 @@ import joblib
 import sklearn as sk
 
 from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import StandardScaler
 
 #importing data into Dataframe
 df = pd.read_csv("Project 1 Data.csv")
 
-#prep for strat. data
+#prep for shuffle data
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=501)
 
-#strat. the data
+#splitting to 20% and 80% data
 for train_index ,test_index in split.split(df,df["Step"]):
     strat_train_set = df.loc[train_index].reset_index(drop=True)
     strat_test_set = df.loc[test_index].reset_index(drop=True)
     
+#setting to independent variable, and dependent variable
+train_y = strat_train_set['Step']
+df_X = strat_train_set.drop(columns = ["Step"])
+   
+#scaling data 
+scaler = StandardScaler()
+scaler.fit(df_X)
+scaled_data = scaler.transform(df_X)
+scaled_data_df=pd.DataFrame(scaled_data, columns=df_X.columns)
+train_X = scaled_data_df
+
+#corr matrix to see corrolation betwn points
+corr_matrix = (train_X).corr()
+sns.heatmap(np.abs(corr_matrix))
+
+
+
