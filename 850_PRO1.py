552e60d5bb5a122b401d68cad769089d7cc9ba45
dhuynh1182 <d11huynh@torontomu.ca>
2023-10-09 16:41:10 -0400

    pro v3
    added data processing function

--- 850_PRO1.py
+++ 850_PRO1.py
@@ -8,6 +8,21 @@ import sklearn as sk
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.preprocessing import StandardScaler
 
+def data_processing(df):
+    # Extract the target variable (train_y) and features (df_X)
+    train_y = df["Step"]
+    df_X = df.drop(columns=["Step"])
+    
+    # Scaling the features
+    scaler = StandardScaler()
+    scaler.fit(df_X)
+    scaled_data = scaler.transform(df_X)
+    scaled_data_df = pd.DataFrame(scaled_data, columns=df_X.columns)
+    print("processed data")
+    
+    return scaled_data_df, train_y
+
+    
 #importing data into Dataframe
 df = pd.read_csv("Project 1 Data.csv")
 
@@ -18,17 +33,8 @@ split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=501)
 for train_index ,test_index in split.split(df,df["Step"]):
     strat_train_set = df.loc[train_index].reset_index(drop=True)
     strat_test_set = df.loc[test_index].reset_index(drop=True)
-    
-#setting to independent variable, and dependent variable
-train_y = strat_train_set['Step']
-df_X = strat_train_set.drop(columns = ["Step"])
-   
-#scaling data 
-scaler = StandardScaler()
-scaler.fit(df_X)
-scaled_data = scaler.transform(df_X)
-scaled_data_df=pd.DataFrame(scaled_data, columns=df_X.columns)
-train_X = scaled_data_df
+
+train_X,train_y = data_processing(strat_train_set)
 
 #corr matrix to see corrolation betwn points
 corr_matrix = (train_X).corr()
@@ -37,11 +43,21 @@ sns.heatmap(np.abs(corr_matrix))
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
-model1 = RandomForestRegressor(n_estimators=30, random_state=50)
+model1 = RandomForestRegressor(n_estimators=50, random_state=50)
 model1.fit(train_X, train_y)
 model1_predictions = model1.predict(train_X)
 model1_train_mae = mean_absolute_error(model1_predictions, train_y)
-print("Model 1 training MAE is: ", round(model1_train_mae,2))
+print(model1_predictions)
+print("Model 1 training MAE is: ", round(model1_train_mae,5))
+
+test_X,test_y = data_processing(strat_test_set)
+
+
+
+
+    
+
+
 
 
 
