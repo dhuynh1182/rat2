4518d83d4df1eff5fa6d38c8c4fceeb2d5bbe207
dhuynh1182 <d11huynh@torontomu.ca>
2023-10-10 11:40:07 -0400

    update 6
    performance matrix and junk

--- 850_PRO1.py
+++ 850_PRO1.py
@@ -7,28 +7,30 @@ import sklearn as sk
 
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, f1_score, precision_score
 
 def data_processing(df):
     # Extract the target variable (train_y) and features (df_X)
-    train_y = df["Step"]
-    df_X = df.drop(columns=["Step"])
+    y = df["Step"]
+    X = df.drop(columns=["Step"])
     
     # Scaling the features
     scaler = StandardScaler()
-    scaler.fit(df_X)
-    scaled_data = scaler.transform(df_X)
-    scaled_data_df = pd.DataFrame(scaled_data, columns=df_X.columns)
+    scaler.fit(X)
+    scaled_data = scaler.transform(X)
+    scaled_data_df = pd.DataFrame(scaled_data, columns=X.columns)
     print("processed data")
     
-    return scaled_data_df, train_y
+    return scaled_data_df, y
 
-from sklearn.metrics import accuracy_score, f1_score, precision_score
+
 
 #for performance analysis
 def getScores(true,pred):
     print("Precision score: ", precision_score(true, pred, average= 'micro'))
     print("Accuracy score: ", accuracy_score(true, pred))
     print("F1 score: ",f1_score(true, pred, average= 'micro'))
+    
     
     return None
 
@@ -59,13 +61,13 @@ test_X,test_y = data_processing(strat_test_set)
 
 #FOR GRID SEARCH PARAMETERS AND MODELS
 from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
-
-
+# #COMMENTED OUT FOR FASTER DEBUGGING
 # # model 1 random forest
-# m1 = RandomForestRegressor(random_state = 501)
+# m1 = RandomForestClassifier(random_state = 501)
 
 # params1 = {
 #     'n_estimators': [10,50,100],
@@ -81,6 +83,8 @@ from sklearn.svm import SVC
 # best_params = grid_search.best_params_
 # print("Best Hyperparameters:", best_params)
 # best_m1 = grid_search.best_estimator_
+
+
 
 #model 2 Support vector machine
 m2 = SVC(random_state= 501)
@@ -116,21 +120,26 @@ best_m2 = grid_search.best_estimator_
 # print("Best Hyperparameters:", best_params3)
 # best_m3 = grid_search.best_estimator_
 
-#step 5 performance
+#STEP 5: performance
 
-#testing
-
+# #model 1
 # best_m1.fit(train_X,train_y)
-# m1_pred = best_m1.predict(test_X).round()
+# m1_pred = best_m1.predict(test_X)
 
+# print("\n~~scores for random forest model~~\n")
 # getScores(test_y,m1_pred)
 
+
+#model 2
 best_m2.fit(train_X,train_y)
-m2_pred = best_m2.predict(test_X).round()
+m2_pred = best_m2.predict(test_X)
 
 print("\n~~scores for SVC model~~\n")
 getScores(test_y,m2_pred)
+cm = confusion_matrix(test_y, m2_pred)
 
+disp = ConfusionMatrixDisplay(confusion_matrix = cm)
+disp.plot()
 
     
 
