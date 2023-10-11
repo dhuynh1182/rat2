319ea1b0abef9e85ad14ae9b3b49891119cb8bab
dhuynh1182 <d11huynh@torontomu.ca>
2023-10-11 16:30:04 -0400

    update 8?
    fixed scalar data leak, added another model, added confusion matrix, and joblib dump

--- 850_PRO1.py
+++ 850_PRO1.py
@@ -9,19 +9,12 @@ from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, f1_score, precision_score
 
-def data_processing(df):
+def extractor(df):
     # Extract the target variable (train_y) and features (df_X)
     y = df["Step"]
     X = df.drop(columns=["Step"])
     
-    # Scaling the features
-    scaler = StandardScaler()
-    scaler.fit(X)
-    scaled_data = scaler.transform(X)
-    scaled_data_df = pd.DataFrame(scaled_data, columns=X.columns)
-    print("processed data")
-    
-    return scaled_data_df, y
+    return X, y
 
 
 
@@ -49,6 +42,15 @@ sns.heatmap(np.abs(corr_matrix))
 
 #STEP 4: 20, 80 split for testing, and training data.
 #prep for shuffle data
+# Scaling the features
+y = df["Step"]
+X = df.drop(columns=["Step"])
+scaler = StandardScaler()
+scaler.fit(X)
+scaled_data = scaler.transform(X)
+scaled_data_df = pd.DataFrame(scaled_data, columns=X.columns)
+print("scaled data")
+
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=501)
 #splitting to 20% and 80% data
 for train_index ,test_index in split.split(df,df["Step"]):
@@ -56,8 +58,8 @@ for train_index ,test_index in split.split(df,df["Step"]):
     strat_test_set = df.loc[test_index].reset_index(drop=True)
 
 
-train_X,train_y = data_processing(strat_train_set)
-test_X,test_y = data_processing(strat_test_set)
+train_X,train_y = extractor(strat_train_set)
+test_X,test_y = extractor(strat_test_set)
 
 
 #FOR GRID SEARCH PARAMETERS AND MODELS
@@ -65,25 +67,26 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.linear_model import LogisticRegression
 
 #COMMENTED OUT FOR FASTER DEBUGGING
-# model 1 random forest
-# m1 = RandomForestClassifier(random_state = 501)
+#model 1 random forest
+m1 = RandomForestClassifier(random_state = 501)
 
-# params1 = {
-#     'n_estimators': [10,50,100],
-#     'max_depth': [None,5,10,15],
-#     'min_samples_split': [2, 5, 10],
-#     'min_samples_leaf': [1, 2, 4],
-#     'max_features': ['sqrt', 'log2']
-# }
+params1 = {
+    'n_estimators': [10,50,100],
+    'max_depth': [None,5,10,15],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'max_features': ['sqrt', 'log2']
+}
 
-# print("\nrunning grid search for Random Forest Model")
-# grid_search = GridSearchCV(m1, params1, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
-# grid_search.fit(train_X, train_y)
-# best_params = grid_search.best_params_
-# print("Best Hyperparameters:", best_params)
-# best_m1 = grid_search.best_estimator_
+print("\nrunning grid search for Random Forest Model")
+grid_search = GridSearchCV(m1, params1, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
+grid_search.fit(train_X, train_y)
+best_params = grid_search.best_params_
+print("Best Hyperparameters:", best_params)
+best_m1 = grid_search.best_estimator_
 
 
 
@@ -103,32 +106,47 @@ best_params2 = grid_search.best_params_
 print("Best Hyperparameters:", best_params2)
 best_m2 = grid_search.best_estimator_
 
-#model 3 
-m3 = sk.tree.DecisionTreeClassifier(random_state = 501)
+# #model 3 
+# m3 = sk.tree.DecisionTreeClassifier(random_state = 501)
 
-params3 = {
-    'criterion': ['gini','entropy','log_loss'],
-    'splitter': ['best','random'],
-    'max_depth': [None,1,2,3],
-    'min_samples_split': [2, 5],
-    'min_samples_leaf': [1, 2, 4],
-    'max_features': [1,'sqrt', 'log2']
+# params3 = {
+#     'criterion': ['gini','entropy','log_loss'],
+#     'splitter': ['best','random'],
+#     'max_depth': [None,1,2,3],
+#     'min_samples_split': [2, 5],
+#     'min_samples_leaf': [1, 2, 4],
+#     'max_features': [1,'sqrt', 'log2']
+# }
+# print("\nrunning grid search for DTC Model")
+# grid_search = GridSearchCV(m3, params3, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
+# grid_search.fit(train_X, train_y)
+# best_params3 = grid_search.best_params_
+# print("Best Hyperparameters:", best_params3)
+# best_m3 = grid_search.best_estimator_
+
+#model 4 logistic
+m4 = LogisticRegression(random_state = 501)
+
+params4 = {
+    'C':[1,2,3,4,5],
+    'max_iter':[5000,6000,8000],
+    'solver':['newton-cg','sag','saga']
 }
-print("\nrunning grid search for DTC Model")
-grid_search = GridSearchCV(m3, params3, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
+print("\nrunning grid search for Logi Model")
+grid_search = GridSearchCV(m4, params4, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
 grid_search.fit(train_X, train_y)
-best_params3 = grid_search.best_params_
-print("Best Hyperparameters:", best_params3)
-best_m3 = grid_search.best_estimator_
+best_params4 = grid_search.best_params_
+print("Best Hyperparameters:", best_params4)
+best_m4 = grid_search.best_estimator_
 
 #STEP 5: performance
 
-#model 3
-best_m3.fit(train_X,train_y)
-m3_pred = best_m3.predict(test_X)
+#model 1
+best_m1.fit(train_X,train_y)
+m1_pred = best_m1.predict(test_X)
 
 print("\n~~scores for random forest model~~\n")
-getScores(test_y,m3_pred)
+getScores(test_y,m1_pred)
 
 
 #model 2
@@ -137,14 +155,33 @@ m2_pred = best_m2.predict(test_X)
 
 print("\n~~scores for SVC model~~\n")
 getScores(test_y,m2_pred)
+
+    
+
+# #model 3
+# best_m3.fit(train_X,train_y)
+# m3_pred = best_m3.predict(test_X)
+
+# print("\n~~scores for DTC model~~\n")
+# getScores(test_y,m3_pred)
+
+
+#model 4
+best_m4.fit(train_X,train_y)
+m4_pred = best_m4.predict(test_X)
+
+print("\n~~scores for logi model~~\n")
+getScores(test_y,m4_pred)
+
+
+#Based on the outputted scores, model 2: SVC is the best model, so a confusion matrix is
+#needed 
 cm = confusion_matrix(test_y, m2_pred)
 
 disp = ConfusionMatrixDisplay(confusion_matrix = cm)
 disp.plot()
 
-    
-
-
-
-
-
+#STEP 6:
+print("\ndumping model 2 into joblib file")
+jb.dump(best_m2,"best_model.joblib")
+        
